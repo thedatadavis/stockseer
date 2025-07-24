cd6cf22010f3641809a607038726acfcd3de3e9e
Chris Davis <ChristopherGDavis@gmail.com>
2025-07-24 07:57:11 +0000

    Can you add google analytics to this project?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { GoogleAnalytics } from '@/components/google-analytics';
 
 export const metadata: Metadata = {
   title: 'Stockseer',
@@ -20,6 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
+        <GoogleAnalytics gaId="497989767" />
         {children}
         <Toaster />
       </body>
